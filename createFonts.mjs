29e7d15f0bd25d228086e19bf739a48490252ef2
bawoma <marcin.bawolski@f-secure.com>
2020-07-19 14:54:07 +0200

    replace click method to mouse API

--- createFonts.mjs
+++ createFonts.mjs
@@ -16,6 +16,8 @@ export default class CreateFonts
     this.selctor = 'button[mi-popup="focus"]';
     this.files   = new readFiles().files;
     this.paths   = new readFiles().paths;
+    this.x       = 826;
+    this.y       = 272;
   }
 
   async uploadImages()
@@ -23,9 +25,11 @@ export default class CreateFonts
     console.log(`Number of images: ${this.files.length}`);
     this.browser = await puppeteer.launch({
       'headless': false,
-      args: ['--start-fullscreen']
+      // args: ['--start-fullscreen'],
     });
     this.page    = await this.browser.newPage();
+
+    // await this.page.setViewport({ width: 1920, height: 1080 });
     
     await this.page.goto(this.link, {
       waitUntil: 'networkidle2',
@@ -40,26 +44,32 @@ export default class CreateFonts
       ]);
       await fileChooser.accept([...this.paths]);
       await this.page.waitForSelector('.icon' , {visible: true});
-      await this.page.waitFor(3000);
-      this.files.forEach(async (item, index) => {
-        await this.page.waitFor(100);
-        await this.page.waitForSelector(`[data-idx="${index}"]` , {visible: true});
-        try {
-          await this.page.click(`[data-idx="${index}"]`);
-        } catch(err) {
-          console.log(err);
-        }
-      })
-      await this.page.click('a[href="#/select/font"]');
-      await this.page.waitForSelector('.btn4' , {visible: true});
-      await this.page.click('.btn4');
-      await this.page.waitFor(3000);
+      
+      // this.files.forEach(async (item, index) => {
+      //   await this.page.waitFor(100);
+      //   await this.page.waitForSelector(`[data-idx="${index}"]` , {visible: true});
+      //   try {
+      //     await this.page.click(`[data-idx="${index}"]`);
+      //   } catch(err) {
+      //     console.log(err);
+      //   }
+      // })
+      await this.page.waitForSelector('button.btn5' , {visible: true});
+      // await this.page.click('button.btn5');
+      await this.page.waitFor(1000);
+      await this.page.mouse.click(747, 140, { button: 'left' });
+      await this.page.waitFor(100);
+      await this.page.mouse.click(this.x, this.y, { button: 'left' });
+      // await this.page.click('a[href="#/select/font"]');
+      // await this.page.waitForSelector('.btn4' , {visible: true});
+      // await this.page.click('.btn4');
+      // await this.page.waitFor(3000);
     
     } catch(err) {
       console.log(err);
-      await this.browser.close();
+      // await this.browser.close();
     }
-    await this.browser.close();
+    // await this.browser.close();
   }
 }
 
